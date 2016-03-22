7e5e87fb18cdea19abde2b1100d687ad18350299
racoder <ra_coder@yahoo.com>
2016-03-22 15:44:49 +0200

    first sidebar version

--- includes/templates/frontend.tpl.php
+++ includes/templates/frontend.tpl.php
@@ -41,13 +41,53 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 	</script>
 
 
+	<!-- the composer sidebar -->
+	<div id="tdc-sidebar">
+		<div class="tdc-top-buttons">
+			<a href="#" class="tdc-add-element">Add element</a>
+			<a href="#" class="tdc-save-page">Save page</a>
+		</div>
 
 
-	<div id="tdc-settings">
-		<div id="tdc-add">Add</div>
-		<div id="tdc-save">Save</div>
+		<!-- breadcrumbs browser -->
+		<div class="tdc-breadcrumbs">
+			<div class="tdc-breadcrumbs-path">
+				<a href="#">row</a>
+				<span class="tdc-breadcrumb-arrow"></span>
+				<a href="#">column</a>
+			</div>
+			<div class="tdc-current-element">
+				Block 14
+			</div>
+		</div>
+
+
+		<!-- the inspector -->
+		<div class="tdc-inspector">
+
+		</div>
+
+
+
+		<!-- modal window -->
+		<div class="tdc-sidebar-modal tdc-modal-open">
+			<div class="tdc-sidebar-modal-title">
+				Add Element
+				<a href="#" class="tdc-modal-close"></a>
+			</div>
+			<div class="tdc-sidebar-modal-content">
+				<!-- sidebar elements list -->
+				<div class="tdc-sidebar-elements">
+					<div class="tdc-sidebar-element">Block 1</div>
+					<div class="tdc-sidebar-element">Block 2</div>
+				</div>
+			</div>
+		</div>
+
 	</div>
 
+
+	<!-- The live iFrame loads in this wrapper :) -->
 	<div id="tdc-live-iframe-wrapper"></div>
 
 	<div style="height: 1px; visibility: hidden; overflow: hidden;">
