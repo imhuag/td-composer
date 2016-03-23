ff6e4dacbcb32ab0a08aef2e21684173e71ff052
racoder <ra_coder@yahoo.com>
2016-03-23 14:45:34 +0200

    sidebar style

--- assets/js/tdcSidebar.js
+++ assets/js/tdcSidebar.js
@@ -9,8 +9,10 @@ var tdcSidebar = {};
 
 (function(){
     tdcSidebar = {
-        init: function () {
 
+
+
+        sidebarModal: function () {
             // Sidebar elements modal window - open
             jQuery('.tdc-add-element').click(function(){
                 jQuery('.tdc-sidebar-modal-elements').addClass('tdc-modal-open');
@@ -20,10 +22,37 @@ var tdcSidebar = {};
             jQuery('.tdc-modal-close').click(function(){
                 jQuery('.tdc-sidebar-modal-elements').removeClass('tdc-modal-open');
             });
+        },
 
 
+
+
+        liveInspectorTabs: function () {
+
+            jQuery('.tdc-tabs a').on('click', function(){
+                if (jQuery(this).hasClass('tdc-tab-active') === true) {
+                    return;
+                }
+
+
+                // the tab
+                jQuery('.tdc-tabs a').removeClass('tdc-tab-active');
+                jQuery(this).addClass('tdc-tab-active');
+
+
+                // content - remove all visible classes
+                jQuery('.tdc-tab-content-wrap .tdc-tab-content').removeClass('tdc-tab-content-visible');
+
+                // add the class to the good content
+                var tabContentId = jQuery(this).data('tab-id');
+                jQuery('#' + tabContentId).addClass('tdc-tab-content-visible');
+            });
         }
+
+
+
     };
 
-    tdcSidebar.init();
+    tdcSidebar.sidebarModal();
+    tdcSidebar.liveInspectorTabs();
 })();
--- includes/templates/frontend.tpl.php
+++ includes/templates/frontend.tpl.php
@@ -64,7 +64,30 @@ require_once( ABSPATH . 'wp-admin/admin-header.php' );
 
 		<!-- the inspector -->
 		<div class="tdc-inspector">
-
+			<div class="tdc-tabs">
+				<a href="#" data-tab-id="td-tab-id-1" class="tdc-tab-active">General</a>
+				<a href="#" data-tab-id="td-tab-id-2">Filter</a>
+				<a href="#" data-tab-id="td-tab-id-3">Ajax filter</a>
+				<a href="#" data-tab-id="td-tab-id-4">Pagination</a>
+			</div>
+			<div class="tdc-tab-content-wrap">
+				<div class="tdc-tab-content tdc-tab-content-visible" id="td-tab-id-1">
+					Bacon ipsum dolor amet brisket tenderloin short ribs leberkas ground round bacon. Swine fatback ribeye tail. Cupim ball tip shankle kevin tail turkey. Pastrami t-bone turkey meatloaf pancetta bacon picanha turducken salami landjaeger flank filet mignon beef. Strip steak swine shank brisket venison.
+					Boudin pig strip steak ball tip landjaeger tongue. Tongue ground round meatball, cupim bresaola venison tail capicola ham hock kevin chuck swine. Jowl rump shank turducken porchetta. T-bone biltong sirloin alcatra andouille shank.
+				</div>
+				<div class="tdc-tab-content" id="td-tab-id-2">
+					Leberkas shank venison meatball turducken frankfurter tenderloin shoulder. Leberkas turkey spare ribs shankle ribeye sausage. Frankfurter pork ribeye tenderloin filet mignon jerky boudin pig tri-tip chuck brisket jowl. Pork chop flank tri-tip tenderloin kevin pork loin landjaeger biltong, turkey alcatra pastrami ham hock swine drumstick jowl. Salami alcatra chuck biltong, leberkas ham hock chicken ground round andouille pancetta beef bacon short ribs strip steak shoulder. Sausage doner strip steak filet mignon. Pork chop bresaola bacon hamburger boudin filet mignon capicola kielbasa pig venison short loin cupim jerky shoulder.
+					Salami porchetta leberkas, pancetta ground round frankfurter shoulder tenderloin andouille shankle ham picanha. Sausage shoulder short ribs, brisket cupim tri-tip hamburger beef. Kielbasa shank pastrami, landjaeger rump leberkas ribeye ham venison tri-tip tenderloin. Jerky chicken ham andouille, jowl sausage venison salami. Spare ribs pork chop pork loin, prosciutto beef ribs tri-tip capicola salami. Porchetta t-bone meatball salami filet mignon short ribs.
+				</div>
+				<div class="tdc-tab-content" id="td-tab-id-3">
+					Salami porchetta leberkas, pancetta ground round frankfurter shoulder tenderloin andouille shankle ham picanha. Sausage shoulder short ribs, brisket cupim tri-tip hamburger beef. Kielbasa shank pastrami, landjaeger rump leberkas ribeye ham venison tri-tip tenderloin. Jerky chicken ham andouille, jowl sausage venison salami. Spare ribs pork chop pork loin, prosciutto beef ribs tri-tip capicola salami. Porchetta t-bone meatball salami filet mignon short ribs.
+					Does your lorem ipsum text long for something a little meatier? Give our generator a try… it’s tasty!
+				</div>
+				<div class="tdc-tab-content" id="td-tab-id-4">
+					Turkey prosciutto short loin chicken pork loin turducken. Doner beef turducken, biltong pastrami pork prosciutto short ribs tri-tip frankfurter brisket leberkas. Hamburger doner venison tongue chicken shank pig sirloin corned beef filet mignon rump alcatra. Frankfurter venison porchetta strip steak short ribs.
+					Swine prosciutto picanha, flank corned beef meatloaf turkey chicken hamburger short loin. Salami meatball prosciutto tongue pork. Pork chop biltong boudin bresaola, tail tongue pancetta prosciutto sausage. Pork belly landjaeger cupim cow shoulder, tail tenderloin. Brisket picanha pork porchetta flank chicken ham bresaola capicola beef cupim jowl short loin. Turkey biltong spare ribs corned beef, shoulder pancetta filet mignon hamburger meatball.
+				</div>
+			</div>
 		</div>
 
 
