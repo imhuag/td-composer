5c700b12a388f1a1ceb9a8e8372eebef4c4add69
racoder <ra_coder@yahoo.com>
2016-07-05 09:35:33 +0300

    TDC_USE_LESS comments

--- td_deploy_mode.php
+++ td_deploy_mode.php
@@ -8,7 +8,8 @@ if (!defined('TD_DEPLOY_MODE')) {
 
 
 /**
- *
+ * @see td_less_style.css.php for the full list of files that are compiled. set this to false and you can use your own
+ * compiler for the less files
  */
 if (!defined('TDC_USE_LESS')) {
 	define("TDC_USE_LESS", true);
